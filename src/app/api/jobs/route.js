462be207bf811c06e5b7db9aba2ae7a6a1d57c30
Abdullah <xzombozz@gmail.com>
2024-08-15 16:54:04 +1000

    supa

--- src/app/api/jobs/route.js
+++ src/app/api/jobs/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server';
-import { Client } from 'pg';
-import { supabase } from '@/lib/supabaseClient';
+// import { supabase } from '@/lib/supabaseClient';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export async function GET() {
   try {
