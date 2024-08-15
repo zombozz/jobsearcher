d4d655d0a75c445e5f15f066d226f871f2ebfbf6
Abdullah <xzombozz@gmail.com>
2024-08-15 10:50:12 +1000

    redir

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -12,8 +12,6 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
-console.log(firebaseConfig.apiKey)
-
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
