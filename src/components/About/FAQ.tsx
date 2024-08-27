ec4cd960f7cbe7f555091e180111eb3061772067
Abdullah <xzombozz@gmail.com>
2024-08-27 11:20:07 +1000

    reworked themes

--- src/components/About/FAQ.tsx
+++ src/components/About/FAQ.tsx
@@ -21,7 +21,7 @@ const FAQ = () => {
               How do I apply for a job through your site?
             </h3>
             <p className="text-lg leading-relaxed">
-              Applying for a job is simple. Once you find a listing that interests you, click on the "Apply" button and follow the prompts to submit your application. You can upload your resume, cover letter, and any other required documents directly through our platform.
+              Applying for a job is simple. Once you find a listing that interests you, click on the &quot;Apply&quot; button and follow the prompts to submit your application. You can upload your resume, cover letter, and any other required documents directly through our platform.
             </p>
           </div>
           <div className="bg-base-200 p-8 rounded-lg shadow-lg">
@@ -29,7 +29,7 @@ const FAQ = () => {
               How can employers post job openings?
             </h3>
             <p className="text-lg leading-relaxed">
-              Employers can easily post job openings by creating an account on our platform. Once logged in, you can navigate to the "Post a Job" section, fill out the necessary details about the position, and publish your listing. Our team is available to assist you with any questions during the process.
+              Employers can easily post job openings by creating an account on our platform. Once logged in, you can navigate to the &quot;Post a Job&quot; section, fill out the necessary details about the position, and publish your listing. Our team is available to assist you with any questions during the process.
             </p>
           </div>
           <div className="bg-base-200 p-8 rounded-lg shadow-lg">
@@ -45,7 +45,7 @@ const FAQ = () => {
               What should I do if I encounter issues on the site?
             </h3>
             <p className="text-lg leading-relaxed">
-              If you experience any issues with our site, please contact our support team through the "Contact Us" page. We are committed to providing prompt assistance and resolving any problems you may encounter.
+              If you experience any issues with our site, please contact our support team through the &quot;Contact Us&quot; page. We are committed to providing prompt assistance and resolving any problems you may encounter.
             </p>
           </div>
         </div>
